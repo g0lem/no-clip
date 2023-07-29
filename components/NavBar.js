19aa87fa80efcfdc4ddc52b7185a4ddeb71294e4
g0lem <sasteve999@gmail.com>
2023-07-29 12:13:36 +0300

    added some css

---
+++ components/NavBar.js
@@ -0,0 +1,14 @@
+import * as elements from 'typed-html';
+import { Logo } from './Logo';
+
+
+export const NavBar = ({}) => {
+    return (
+        <div class="NavBar">
+            <Logo/>
+            <div class='NavBarItem'>
+                Test
+            </div>
+        </div>
+    )
+}
--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -1,27 +1,41 @@
 
 import * as elements from 'typed-html';
+import { GameTile } from '../../components/GameTile';
+import { NavBar } from '../../components/NavBar';
 
-// .get('/2048', ({html}) => html(<Game2048/>))
-// .get('/clumsy-bird', ({html}) => html(<ClumsyBird/>))
-// .get('/hextris', ({html}) => html(<Hextris/>))
-// .get('/astray', ({html}) => html(<Astray/>))
+
+const gameList = [
+    {
+        name: '2048',
+        path: '/2048',
+        imagePreview: 'https://superdevresources.com/wp-content/uploads/2014/09/2048-open-source-game.png'
+    },
+    {
+        name: 'Clumsy Bird',
+        path: '/clumsy-bird',
+        imagePreview: 'https://superdevresources.com/wp-content/uploads/2014/09/clumsy-bird-open-source-game.png'
+    },
+    {
+        name: 'Hextris',
+        path: '/hextris',
+        imagePreview: 'https://superdevresources.com/wp-content/uploads/2014/09/hextris-open-source-web-game.png'
+    },
+    {
+        name: 'Astray',
+        path: '/astray',
+        imagePreview: 'https://superdevresources.com/wp-content/uploads/2014/09/astray-webgl-maze-game-open-source.jpg'
+    },
+]
 
 
 export const Home = (props) => {
-    return <div>
-        <div>
-            <a href='/2048'>2048</a>
+    return <div class='Homepage'>
+        <NavBar/>
+        <div class='GameList'>
+        {
+            gameList.map(elm=><GameTile {...elm}/>)
+        }
         </div>
-        <div>
-            <a href='/clumsy-bird'>ClumsyBird</a>
-        </div>
-
-        <div>
-            <a href='/hextris'>Hextris</a>
-        </div>
-
-        <div>
-            <a href='/astray'>Astray</a>
-        </div>
+        <link rel="stylesheet" href="public/home.css"/>
     </div>
 }
