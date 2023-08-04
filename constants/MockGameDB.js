98cea0b04d535cf442f9d8c2e034bc45a9957c36
g0lem <sasteve999@gmail.com>
2023-08-04 12:44:57 +0300

    added more games

--- constants/MockGameDB.js
+++ constants/MockGameDB.js
@@ -27,16 +27,14 @@ export const ExternalGamesDB = [
         name: 'Ninja vs Evil Corp',
         path: '/ninja-vs-evil-corp',
         src: 'https://js13kgames.com/games/ninja-vs-evilcorp/index.html',
-        imagePreview: 'https://superdevresources.com/wp-content/uploads/2014/09/freeciv-web-screenshot.jpg'
+        imagePreview: 'https://github.com/remvst/ninja/raw/master/assets/main-menu-1600x800.png'
     },
-
     {
         name: 'Ancient Beast',
         path: '/ancient-beast',
         src: 'https://play.AncientBeast.com"',
-        imagePreview: 'https://superdevresources.com/wp-content/uploads/2014/09/freeciv-web-screenshot.jpg'
+        imagePreview: 'https://camo.githubusercontent.com/73a15c42a2e0f49ae4b9734a8c4a197221dcedf412464ea241b9f479e6edfe84/68747470733a2f2f7261772e6769746875622e636f6d2f467265657a696e674d6f6f6e2f416e6369656e7442656173742d576562736974652f6d61737465722f6d756c74696d656469612f73637265656e73686f74732f76302e332532304461726b253230466f726573742e6a7067'
     },
-
     {
         name: 'Tower Game',
         path: '/tower-game',
@@ -47,9 +45,8 @@ export const ExternalGamesDB = [
         name: 'Duck Hunt',
         path: '/duck-hunt',
         src: 'https://duckhuntjs.com/',
-        imagePreview: 'https://d3frsattnbx5l6.cloudfront.net/1539284406589-towergame1.png'
+        imagePreview: 'https://miro.medium.com/v2/resize:fit:1000/1*CHpGqF5_AjZCgQokepOIHQ.jpeg'
     },
-
     {
         name: 'Space Block Invaders',
         path: '/space-block-invaders',
@@ -63,20 +60,88 @@ export const ExternalGamesDB = [
         src: 'https://js13kgames.com/games/q1k3/index.html',
         imagePreview: 'https://js13kgames.com/games/q1k3/__big.jpg'
     },
-
     {
         name: 'Minipunk',
         path: '/minipunk',
         src: 'https://js13kgames.com/games/minipunk/index.html',
-        imagePreview: 'https://js13kgames.com/games/q1k3/__big.jpg'
+        imagePreview: 'https://camo.githubusercontent.com/66d8476224605b9ee47de5ab604f7d735e725ad3c51581bdf191b5eb75e5f119/68747470733a2f2f636f64792e6562626572736f6e2e636f6d2f6d696e6970756e6b2f73637265656e73686f74732f70726f6367656e2d73637265656e73686f742e6a7067'
     },
-
-
     {
         name: 'The Last Spartan',
         path: '/the-last-spartan',
         src: 'https://js13kgames.com/games/the-last-spartan/index.html',
-        imagePreview: 'https://js13kgames.com/games/q1k3/__big.jpg'
+        imagePreview: 'https://github.com/ferronsays/js13k-TheLastSpartan/raw/master/media/logo_large_enlarged.png'
     },
-
+    {
+        name: 'Dwarfs: There and Back Again',
+        path: '/dwarfs-there-and-back-again',
+        src: 'https://js13kgames.com/games/dwarfs-there-and-back-again/index.html',
+        imagePreview: 'https://js13kgames.com/games/dwarfs-there-and-back-again/__big.jpg'
+    },
+    {
+        name: 'Swagshot',
+        path: '/swagshot',
+        src: 'https://js13kgames.com/games/swagshot/index.html',
+        imagePreview: 'https://js13kgames.com/games/swagshot/__big.jpg'
+    },
+    {
+        name: 'Highway 404',
+        path: '/highway-404',
+        src: 'https://js13kgames.com/games/highway-404/index.html',
+        imagePreview: 'https://js13kgames.com/games/highway-404/__big.jpg'
+    },
+    {
+        name: 'The Wandering Wraith',
+        path: '/the-wandering-wraith',
+        src: 'https://js13kgames.com/games/the-wandering-wraith/index.html',
+        imagePreview: 'https://js13kgames.com/games/the-wandering-wraith/__big.jpg'
+    },
+    {
+        name: 'UNDERRUN',
+        path: '/underrrun',
+        src: 'https://js13kgames.com/games/underrun/index.html',
+        imagePreview: 'https://js13kgames.com/games/underrun/__big.jpg'
+    },
+    {
+        name: 'The Matr13k',
+        path: '/underrrun',
+        src: 'https://js13kgames.com/games/the-matr13k/index.htmll',
+        imagePreview: 'https://js13kgames.com/games/the-matr13k/__big.jpg'
+    },
+    {
+        name: 'Spacecraft',
+        path: '/spacecraft',
+        src: 'https://js13kgames.com/games/spacecraft/index.html',
+        imagePreview: 'https://js13kgames.com/games/spacecraft/__big.jpg'
+    },
+    {
+        name: 'Re-wire',
+        path: '/re-wire',
+        src: 'https://js13kgames.com/games/re-wire/index.html',
+        imagePreview: 'https://js13kgames.com/games/re-wire/__big.jpg'
+    },
+    {
+        name: 'The Chroma Incident',
+        path: '/the-chroma-incident',
+        src: 'https://js13kgames.com/games/the-chroma-incident/index.html',
+        imagePreview: 'https://js13kgames.com/games/the-chroma-incident/__big.jpg'
+    },
+    {
+        name: 'Push Back',
+        path: '/push-back',
+        src: 'https://js13kgames.com/games/push-back/index.html',
+        imagePreview: 'https://js13kgames.com/games/push-back/__big.jpg'
+    },
+    {
+        name: 'oh, flip!',
+        path: '/oh-flip',
+        src: 'https://js13kgames.com/games/oh-flip/index.html',
+        imagePreview: 'https://js13kgames.com/games/oh-flip/__big.jpg'
+    },
+    {
+        name: 'Wander',
+        path: '/wander',
+        src: 'https://js13kgames.com/games/wander/index.html',
+        imagePreview: 'https://js13kgames.com/games/wander/__big.jpg'
+    },
 ]
