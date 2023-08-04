f9a4f73b332bba4cba94f4203ae53b436fcd20c7
g0lem <sasteve999@gmail.com>
2023-08-04 11:37:27 +0300

    reduced code and made things nicer

---
+++ constants/MockGameDB.js
@@ -0,0 +1,82 @@
+export const ExternalGamesDB = [
+    {
+        name: '2048',
+        path: '/2048',
+        src: 'https://4ark.me/2048/',
+        imagePreview: 'https://superdevresources.com/wp-content/uploads/2014/09/2048-open-source-game.png'
+    },
+    {
+        name: 'Clumsy Bird',
+        path: '/clumsy-bird',
+        src: 'http://ellisonleao.github.io/clumsy-bird/',
+        imagePreview: 'https://superdevresources.com/wp-content/uploads/2014/09/clumsy-bird-open-source-game.png'
+    },
+    {
+        name: 'Hextris',
+        path: '/hextris',
+        src: 'https://hextris.io/',
+        imagePreview: 'https://superdevresources.com/wp-content/uploads/2014/09/hextris-open-source-web-game.png'
+    },
+    {
+        name: 'Astray',
+        path: '/astray',
+        src: 'https://wwwtyro.github.io/Astray/',
+        imagePreview: 'https://superdevresources.com/wp-content/uploads/2014/09/astray-webgl-maze-game-open-source.jpg'
+    },
+    {
+        name: 'Ninja vs Evil Corp',
+        path: '/ninja-vs-evil-corp',
+        src: 'https://js13kgames.com/games/ninja-vs-evilcorp/index.html',
+        imagePreview: 'https://superdevresources.com/wp-content/uploads/2014/09/freeciv-web-screenshot.jpg'
+    },
+
+    {
+        name: 'Ancient Beast',
+        path: '/ancient-beast',
+        src: 'https://play.AncientBeast.com"',
+        imagePreview: 'https://superdevresources.com/wp-content/uploads/2014/09/freeciv-web-screenshot.jpg'
+    },
+
+    {
+        name: 'Tower Game',
+        path: '/tower-game',
+        src: 'https://iamkun.github.io/tower_game/',
+        imagePreview: 'https://d3frsattnbx5l6.cloudfront.net/1539284406589-towergame1.png'
+    },
+    {
+        name: 'Duck Hunt',
+        path: '/duck-hunt',
+        src: 'https://duckhuntjs.com/',
+        imagePreview: 'https://d3frsattnbx5l6.cloudfront.net/1539284406589-towergame1.png'
+    },
+
+    {
+        name: 'Space Block Invaders',
+        path: '/space-block-invaders',
+        src: 'https://js13kgames.com/games/space-block-invaders/index.html',
+        imagePreview: 'https://js13kgames.com/games/space-block-invaders/__big.jpg'
+    },
+
+    {
+        name: 'Q1K3',
+        path: '/q1k3',
+        src: 'https://js13kgames.com/games/q1k3/index.html',
+        imagePreview: 'https://js13kgames.com/games/q1k3/__big.jpg'
+    },
+
+    {
+        name: 'Minipunk',
+        path: '/minipunk',
+        src: 'https://js13kgames.com/games/minipunk/index.html',
+        imagePreview: 'https://js13kgames.com/games/q1k3/__big.jpg'
+    },
+
+
+    {
+        name: 'The Last Spartan',
+        path: '/the-last-spartan',
+        src: 'https://js13kgames.com/games/the-last-spartan/index.html',
+        imagePreview: 'https://js13kgames.com/games/q1k3/__big.jpg'
+    },
+
+]
--- pages/Games/2048/Game2048.js
+++
@@ -1,9 +0,0 @@
-//https://play2048.co/
-import * as elements from 'typed-html';
-import { Game } from '../../../components/Game';
-
-
-
-export const Game2048 = (props) => {
-    return <Game src="https://4ark.me/2048/" title="2048"/>
-}
