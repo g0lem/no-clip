e9f46edb444d67889deb3e97472e83b2593f88f1
g0lem <sasteve999@gmail.com>
2023-08-06 12:48:19 +0300

    fixed doctype & disabled GA on localhost

--- components/BasePage.js
+++ components/BasePage.js
@@ -5,32 +5,33 @@ import { NavBar } from './NavBar';
 
 
 export const BasePage = ({children}) => {
-    return (
+    return [
+        "<!DOCTYPE html>",
         <html>
-            <head>
-                <link rel="stylesheet" href="/public/home.css"/>
-                <link rel="stylesheet" href="/public/game.css"/>
-                <link rel="stylesheet" href="/public/logo.css"/>
+        <head>
+            <link rel="stylesheet" href="/public/home.css"/>
+            <link rel="stylesheet" href="/public/game.css"/>
+            <link rel="stylesheet" href="/public/logo.css"/>
 
 
-                <meta property="og:title" content=".no-clip" />
-                <meta property="og:type" content="website:game" />
-                <meta property="og:url" content="https://no-clip.io/" />
-                <meta property="og:image" content="https://no-clip.io/public/preview.png" />
-                <meta property="og:description" content="Reproducing flash era nostalgia with new and improved technology since 2023"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </head>
-            
-            <body>
-                <div class='Homepage'>
-                    <NavBar/>
-                    {children}
-                </div>  
-            </body>
+            <meta property="og:title" content=".no-clip" />
+            <meta property="og:type" content="website:game" />
+            <meta property="og:url" content="https://no-clip.io/" />
+            <meta property="og:image" content="https://no-clip.io/public/preview.png" />
+            <meta property="og:description" content="Reproducing flash era nostalgia with new and improved technology since 2023"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </head>
+        
+        <body>
+            <div class='Homepage'>
+                <NavBar/>
+                {children}
+            </div>  
+        </body>
 
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-S3LHH1FWKH"></script>
-            <script src="public/scripts/google-analytics.js"/>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S3LHH1FWKH"></script>
+        <script src="public/scripts/google-analytics.js"/>
 
-        </html>
-)
+    </html>
+    ]
 }
--- public/scripts/google-analytics.js
+++ public/scripts/google-analytics.js
@@ -1,6 +1,11 @@
 
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+const { location: _location } = window;
 
-gtag('config', 'G-S3LHH1FWKH');
+
+if(!_location.toString().includes('localhost')) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-S3LHH1FWKH');
+}
