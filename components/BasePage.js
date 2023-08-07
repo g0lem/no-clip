b0cdf5f9fbd9d10b3bce417c38ff5db454fe46c2
g0lem <sasteve999@gmail.com>
2023-08-07 09:37:23 +0300

    added lang attribute to html

--- components/BasePage.js
+++ components/BasePage.js
@@ -5,29 +5,29 @@ import { NavBar } from './NavBar';
 export const BasePage = ({children}) => {
     return [
         "<!DOCTYPE html>",
-        <html>
-        <head>
-            <meta property="og:title" content=".no-clip" />
-            <meta property="og:type" content="website:game" />
-            <meta property="og:url" content="https://no-clip.io/" />
-            <meta property="og:image" content="https://no-clip.io/public/preview.png" />
-            <meta property="og:description" content="Reproducing flash era nostalgia with new and improved technology since 2023"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <html lang="en">
+            <head>
+                <meta property="og:title" content=".no-clip" />
+                <meta property="og:type" content="website:game" />
+                <meta property="og:url" content="https://no-clip.io/" />
+                <meta property="og:image" content="https://no-clip.io/public/preview.png" />
+                <meta property="og:description" content="Reproducing flash era nostalgia with new and improved technology since 2023"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-            <link rel="stylesheet" href="/public/home.css"/>
-            <link rel="stylesheet" href="/public/game.css"/>
-            <link rel="stylesheet" href="/public/logo.css"/>
-        </head>
-        
-        <body>
-            <div class='Homepage'>
-                <NavBar/>
-                {children}
-            </div>  
+                <link rel="stylesheet" href="/public/home.css"/>
+                <link rel="stylesheet" href="/public/game.css"/>
+                <link rel="stylesheet" href="/public/logo.css"/>
+            </head>
+            
+            <body>
+                <div class='Homepage'>
+                    <NavBar/>
+                    {children}
+                </div>  
 
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-S3LHH1FWKH"></script>
-            <script async src="public/scripts/google-analytics.js"/>
-        </body>
-    </html>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-S3LHH1FWKH"></script>
+                <script async src="public/scripts/google-analytics.js"/>
+            </body>
+        </html>
     ]
 }
